c3481bd531a3c7a94d5c64aed40ae3f8a3cea225
The Dinosaur Guy <dinosaur@dinosaur-guy.com>
2018-08-23 16:44:09 +0200

    Implement First simple solution

--- 01-helloWorld/src/main/java/HelloWorldApplication.java
+++ 01-helloWorld/src/main/java/HelloWorldApplication.java
@@ -1,5 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HelloWorldApplication {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld");
+    public static void main(String[] args) throws IOException {
+        BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("What is your name?");
+        String name = inputReader.readLine();
+        String output = "Hello, " + name + ", nice to meet you!";
+        System.out.println(output);
     }
 }
