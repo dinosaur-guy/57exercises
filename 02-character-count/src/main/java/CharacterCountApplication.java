032a632f054177c949e60ac48ea99fa33aae7e40
The Dinosaur Guy <dinosaur@dinosaur-guy.com>
2018-08-23 20:26:52 +0200

    Implement GUI version of the program

--- 02-character-count/src/main/java/CharacterCountApplication.java
+++ 02-character-count/src/main/java/CharacterCountApplication.java
@@ -1,11 +1,23 @@
+import gui.MainFrame;
+
+import javax.swing.*;
 import java.io.*;
 
 public class CharacterCountApplication {
     public static void main(String[] args) throws IOException {
+        executeGuiProgram();
+    }
+
+    private static void executeConsoleProgram() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         CharacterCountProgram program = new CharacterCountProgram(reader, writer);
         program.execute();
+    }
+
+    private static void executeGuiProgram() {
+        JFrame frame = new MainFrame("Character Counter");
+        frame.setVisible(true);
     }
 }
---
+++ 02-character-count/src/main/java/gui/MainFrame.java
@@ -0,0 +1,59 @@
+package gui;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private JPanel panel;
+    private JLabel inputCharactersLabel;
+    private JTextField inputCharactersField;
+    private JLabel showCharacterCountLabel;
+
+    private String EMPTY_INPUT = "No input string yet.";
+    private String template = "'%s' has %d characters";
+
+
+    public MainFrame(String title) throws HeadlessException {
+        super(title);
+        this.setSize(600, 400);
+
+        panel = new JPanel();
+        inputCharactersField = new JTextField(10);
+        inputCharactersField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updateCharacterCount();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updateCharacterCount();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updateCharacterCount();
+            }
+        });
+
+        inputCharactersLabel = new JLabel("What is your input string?");
+        showCharacterCountLabel = new JLabel(EMPTY_INPUT);
+
+        panel.add(inputCharactersLabel);
+        panel.add(inputCharactersField);
+        panel.add(showCharacterCountLabel);
+
+        this.add(panel);
+    }
+
+    private void updateCharacterCount() {
+        String input = inputCharactersField.getText();
+        if (input.length() <= 0) {
+            showCharacterCountLabel.setText(EMPTY_INPUT);
+        } else {
+            showCharacterCountLabel.setText(String.format(template, input, input.length()));
+        }
+    }
+}
