518b24f39f75e27cacbb749529dc61915665ad5d
The Dinosaur Guy <dinosaur@dinosaur-guy.com>
2018-08-23 17:08:33 +0200

    Implement NoVariable Challenge

--- 01-helloWorld/src/main/java/HelloWorldApplication.java
+++ 01-helloWorld/src/main/java/HelloWorldApplication.java
@@ -4,11 +4,20 @@ import java.io.InputStreamReader;
 
 public class HelloWorldApplication {
     public static void main(String[] args) throws IOException {
+        programNoVariables();
+    }
+
+    private static void program() throws IOException {
         BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("What is your name?");
         String name = inputReader.readLine();
         String output = "Hello, " + name + ", nice to meet you!";
         System.out.println(output);
+    }
+
+    private static void programNoVariables() throws IOException {
+        System.out.println("What is your name?");
+        System.out.println("Hello, " + new BufferedReader(new InputStreamReader(System.in)).readLine() + ", nice to meet you!");
     }
 }
